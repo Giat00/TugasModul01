afea3cbfb5d5c218fabff3ef75a3ad96e43087d7
Giat00 <81604393+Giat00@users.noreply.github.com>
2021-05-30 11:39:16 +0800

    Input dengan InputDialog

--- DataTypes.java
+++ DataTypes.java
@@ -1,23 +1,42 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
 	public static void main(String[] args){
-		String namaDepan = "Giat";
-		String namaTengah = "Adi";
-		String namaBelakang = "Hatmojo";
-		int usia = 18;
-		int targetTahunKuliah = 4;
-		double ipk = 3.89764512;
-		char nilaiAbjad = 'A';
+
+		Scanner scanner = new Scanner(System.in);
+
+		String namaDepan;
+		String namaTengah;
+		String namaBelakang;
+		int usia ;
+		double ipk ;
+		char nilaiAbjad ;
 		boolean tampan = true;
+
+	System.out.println("Masukkan Nama Depan      : ");
+	namaDepan = scanner.nextLine();
+	System.out.println("Masukkan Nama Tengah     : ");
+	namaTengah = scanner.nextLine();
+	System.out.println("Masukkan Nama Tengah     : ");
+	namaBelakang = scanner.nextLine();;
+	System.out.println("Masukkan Usia            : ");
+	usia = scanner.nextInt();
+	System.out.println("Masukkan IPK             : ");
+	ipk = scanner.nextDouble();
+	System.out.println("Masukkan Nilai Abjad     : ");
+	nilaiAbjad = scanner.next().charAt(0);
+	System.out.println("Tampan ?                   : ");
+	tampan = scanner.nextBoolean();
+
+
 	System.out.println("Nama Depan    : "+namaDepan);
 	System.out.println("Nama Tengah   : "+namaTengah);
 	System.out.println("Nama Belakang : "+namaBelakang);
 	System.out.println("Usia	  : "+usia);
-	System.out.println("Target Kuliah : "+targetTahunKuliah );
 	System.out.println("IPK		  : "+ipk);
 	System.out.println("Nilai PBO	  : "+nilaiAbjad);
-	System.out.println("Tampan	  : "+tampan);
+	System.err.println("Tampan	  : "+tampan);
 
 	JOptionPane.showMessageDialog(null, "Hai, "+ namaDepan + namaTengah + namaBelakang);
 
